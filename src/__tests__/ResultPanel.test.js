684423b575553dc78a36e97894456c9d3787a330
Dan Mutton <daniel.mutton@gmail.com>
2017-04-04 11:03:58 +1000

    Add jest config to support mount and test TweetList. Note the warnings that appear due to Mount support for React Native being incomplete

--- src/__tests__/ResultPanel.test.js
+++ src/__tests__/ResultPanel.test.js
@@ -8,7 +8,16 @@ describe('<ResultPanel>', () => {
     expect(rendered.type()).not.toBeNull();
   });
 
-  it('renders an entry for each tweet', () => {
+  it('shows an error when there is an error', () => {
+    const rendered = shallow(
+      <ResultPanel
+        error="There was an error"
+      />);
+    const error = rendered.find('ErrorText');
+    expect(error.props().children).toContain('There was an error');
+  });
+
+  it('renders an tweet list with tweets when there is not an error', () => {
     const data = [
       { id: '1', text: '123' },
       { id: '2', text: '456' },
@@ -17,18 +26,8 @@ describe('<ResultPanel>', () => {
       <ResultPanel
         tweets={data}
       />);
-    const tweets = rendered.find('Tweet');
-    expect(tweets.length).toBe(2);
-    expect(tweets.at(0).prop('tweet')).toEqual(data[0]);
-    expect(tweets.at(1).prop('tweet')).toEqual(data[1]);
-  });
-
-  it('shows an error when there is an error', () => {
-    const rendered = shallow(
-      <ResultPanel
-        error="There was an error"
-      />);
-    const error = rendered.find('ErrorText');
-    expect(error.props().children).toContain('There was an error');
+    const tweetlist = rendered.find('TweetList');
+    expect(tweetlist.length).toBe(1);
+    expect(tweetlist.prop('tweets')).toEqual(data);
   });
 });
---
+++ src/__tests__/TweetList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TweetList from '../TweetList';
+
+describe('<TweetList>', () => {
+  it('renders the component', () => {
+    const rendered = mount(<TweetList tweets={[]} />);
+    expect(rendered.type()).not.toBeNull();
+  });
+
+  it('renders an entry for each tweet', () => {
+    const data = [
+      { id: '1', created_at: '1111', text: '123', user: { name: 'dan' } },
+      { id: '2', created_at: '2222', text: '456', user: { name: 'joe' } },
+    ];
+    const rendered = mount(
+      <TweetList
+        tweets={data}
+      />);
+    const tweets = rendered.find('Tweet');
+    expect(tweets.length).toBe(2);
+    expect(tweets.at(0).prop('tweet')).toEqual(data[0]);
+    expect(tweets.at(1).prop('tweet')).toEqual(data[1]);
+  });
+});
